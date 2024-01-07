3beee7f09ffd451646ddf5dbbdc7e127f6a78973
Maruf Ahmed <marufah76@gmail.com>
2024-01-07 12:57:27 +0600

    feat: add data table pagination

--- frontend/src/components/data-table/data-table.tsx
+++ frontend/src/components/data-table/data-table.tsx
@@ -23,7 +23,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-// import { DataTablePagination } from "../components/data-table-pagination"
+import { DataTablePagination } from "@/components/data-table/data-table-pagination";
 import { DataTableToolbar } from "@/components/data-table/data-table-toolbar";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
@@ -134,7 +134,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      {/* <DataTablePagination table={table} /> */}
+      <DataTablePagination table={table} />
     </div>
   );
 }
